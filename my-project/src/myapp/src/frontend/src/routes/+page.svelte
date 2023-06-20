f8fabed3c5ac13dfec9c1950c722df0f4cb6c42e
goFrendi <gofrendiasgard@gmail.com>
2023-06-21 05:21:39 +0700

    update to 0.0.68

--- my-project/src/myapp/src/frontend/src/routes/+page.svelte
+++ my-project/src/myapp/src/frontend/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <h1 class="text-3xl font-bold underline">Welcome to Myapp</h1>
 
-<p>You can find this page source code on: <code>myapp/src/frontend/src/routes/+page.svelte</code></p>
-<p>To add navigation menu, you can visit: <code>myapp/src/frontend/src/lib/config/navData.ts</code></p>
-<p>Visit <a href="https://kit.svelte.dev/docs/project-structure">Sveltekit Documentation</a> for more information</p>
+<ul>
+    <li>You can find this page source code on: <code>myapp/src/frontend/src/routes/+page.svelte</code></li>
+    <li>To add navigation menu, you can visit: <code>myapp/src/frontend/src/lib/config/navData.ts</code></li>
+    <li>Visit <a href="https://kit.svelte.dev/docs/project-structure">Sveltekit Documentation</a> for more information</li>
+</ul>
